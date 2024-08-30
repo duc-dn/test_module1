cc2244aabb037e0b7fc364902b24fcfa261d8164
ducdn <ducdo23022001@gmail.com>
2024-08-30 23:00:08 +0700

    add module1

--- math/caculate.py
+++ math/caculate.py
@@ -1,10 +1,15 @@
+from loguru import logger
+
 def sum(a: int, b: int) -> int:
+    logger.info(f"sum {a} + {b} = {a + b}")
     return a + b
 
 
 def sub(a: int, b: int) -> int:
+    logger.info(f"sub {a} - {b} = {a - b}")
     return a - b
 
 
 def mul(a: int, b: int) -> int:
+    logger.info(f"mul {a} * {b} = {a * b}")
     return a * b
